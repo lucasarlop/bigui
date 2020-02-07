9414edd80dbca040f284fe8b07f577f25ee33b4d
Lucas Lopes <lucas.arlop@gmail.com>
2020-02-07 09:14:46 -0300

    Add new pet feature

--- app/controllers/pet_controller.rb
+++ app/controllers/pet_controller.rb
@@ -1,7 +1,17 @@
 class PetController < ApplicationController
   respond_to :json
-  
+
   def index
     render json: Pet.all
+  end
+
+  def new
+    render json: { pet: Pet.create(pet_params) }
+  end
+
+  private
+
+  def pet_params
+    params.require(:pet).permit(:name, :breed, :gender, :size, :birthday_date)
   end
 end
